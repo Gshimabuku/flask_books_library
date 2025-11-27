c86b9fe44cbddfb4cca2a8112dd216763645dc2f
Gshimabuku <123082798+Gshimabuku@users.noreply.github.com>
2025-11-27 17:48:35 +0900

    test_program

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Render + Flask デプロイテスト成功！"
+@app.route('/')
+def home():
+return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/list')
+def list_page():
+return render_template('list.html')
+
+@app.route('/detail')
+def detail_page():
+return render_template('detail.html')
+
+if __name__ == '__main__':
+app.run(debug=True)
