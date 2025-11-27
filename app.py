07e40782b845de119ef99ba54953be7c0f418a62
Gshimabuku <123082798+Gshimabuku@users.noreply.github.com>
2025-11-27 17:53:50 +0900

    test_program

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/list')
 def list_page():
-return render_template('list.html')
+    return render_template('list.html')
 
 @app.route('/detail')
 def detail_page():
-return render_template('detail.html')
+    return render_template('detail.html')
 
 if __name__ == '__main__':
-app.run(debug=True)
+    app.run(debug=True)
