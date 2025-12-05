a798164630a9b1b21a4d025a0dd0de7449a5095a
Gshimabuku <123082798+Gshimabuku@users.noreply.github.com>
2025-12-05 15:40:41 +0900

    course

--- app.py
+++ app.py
@@ -5,47 +5,24 @@ from services.notion_service import get_courses
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 # --------------------------
-# ① 環境変数の読み取りテスト
+# コース
 # --------------------------
-@app.route('/debug/env')
-def debug_env():
-    return jsonify({
-        "NOTION_API_KEY": os.getenv("NOTION_API_KEY"),
-        "NOTION_DB_COURSES_ID": os.getenv("NOTION_DB_COURSES_ID"),
-        "NOTION_DB_LAYOUTS_ID": os.getenv("NOTION_DB_LAYOUTS_ID"),
-        "NOTION_DB_HOLES_ID": os.getenv("NOTION_DB_HOLES_ID"),
-    })
-
-# --------------------------
-# ② Notion API の実データ確認
-# --------------------------
-@app.route('/debug/notion')
-def debug_notion():
-    try:
-        data = get_courses()
-        return jsonify({
-            "status": "success",
-            "count": len(data),
-            "data": data
-        })
-
-    except Exception as e:
-        return jsonify({
-            "status": "error",
-            "error_message": str(e)
-        }), 500
-
-# --------------------------
-# コース一覧画面
-# --------------------------
-@app.route('/courses')
+@app.route('/course/list')
 def courses():
     data = get_courses()
-    return render_template('courses.html', courses=data)
+    return render_template('courses/list.html', courses=data)
+
+# --------------------------
+# ラウンド
+# --------------------------
 
 
 if __name__ == '__main__':
